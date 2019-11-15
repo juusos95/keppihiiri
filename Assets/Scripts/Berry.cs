c3a80c9063bb5a21b5bcb9b07008e8d9813463d8
Maistelujaska <50902126+Maistelujaska@users.noreply.github.com>
2019-11-15 13:01:55 +0200

    Berries

---
+++ Assets/Scripts/Berry.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Berry : MonoBehaviour
+{
+    Rigidbody rb;
+    FixedJoint fj;
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Berri"&&collider.GetComponent<BoxCollider>())
+        {
+            fj = gameObject.AddComponent(typeof(FixedJoint)) as FixedJoint;
+            rb = collider.gameObject.GetComponentInChildren(typeof(Rigidbody)) as Rigidbody;
+            fj.connectedBody = rb;
+            fj.connectedMassScale = 0;
+        }
+    }
+    /*private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            collider.transform.parent = null;
+        }
+    }*/
+}
