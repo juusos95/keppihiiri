455889b5d42d1e2f6b5d78c504c23f8aff644432
TuukkaR <56426883+TuukkaR@users.noreply.github.com>
2019-11-15 09:46:42 +0200

    scripts

---
+++ Assets/Scripts/Bip Sripts/Fallingplatforms.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fallingplatforms : MonoBehaviour
+{
+    public float fallDelay = 2.0f;
+
+    public float Respawn = 2.0f;
+    public BoxCollider2D boxCol;
+    public BoxCollider2D boxCol2;
+
+    public Animator animator;
+
+    public Rigidbody2D rbd;
+    
+    public Vector2 initialposition;
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(Respawn);
+        rbd.isKinematic = true;
+        boxCol.enabled = true;
+        boxCol2.enabled = true;
+        transform.position = initialposition;
+        rbd.velocity = Vector2.zero;
+        animator.Play("Platformspawn");
+    }
+
+    private void Start()
+    {
+        initialposition = transform.position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collidedWithThis)
+    {
+        if (collidedWithThis.gameObject.tag == "Player")
+        {
+            StartCoroutine(FallAfterDelay());
+        }
+    }
+
+    IEnumerator FallAfterDelay()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rbd.isKinematic = false;
+        boxCol.enabled = false;
+        boxCol2.enabled = false;
+        StartCoroutine(Spawn());
+
+        animator.Play("Platformrest");
+    }
+}
+
