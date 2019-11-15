45025f905c5167695440d9a3ae6d4fab698a5945
miika <miikv666@gmail.com>
2019-11-15 12:57:29 +0200

    stickjabi

---
+++ Assets/Scripts/StickJab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickJab : MonoBehaviour
+{
+    public Transform player;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.localPosition = new Vector3(player.position.x, player.position.y, player.position.z + 15);
+
+        }
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            collider.transform.parent = null;
+        }*/
+    }
+}
