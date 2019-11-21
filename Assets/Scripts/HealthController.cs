f28ae9acbd73740ed97fef432ff949ba5de12152
JoonasKela <56426885+JoonasKela@users.noreply.github.com>
2019-11-21 12:53:37 +0200

    healthcontroller

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
+    public Image healthBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar = startHealth;
     }
 
     // Update is called once per frame
-    void Update()
+    public void TakeDamage(float amount)
     {
-        
+        healthBar.fillAmount = health / startHealth;
     }
 }
