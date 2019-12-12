cf247b60faff847e46b3020e2bbc00483337dc8a
JoonasKela <56426885+JoonasKela@users.noreply.github.com>
2019-12-12 09:01:44 +0200

    Update StickJab.cs

--- Assets/Scripts/StickJab.cs
+++ Assets/Scripts/StickJab.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System;
 public class StickJab : MonoBehaviour
 {
+<<<<<<< HEAD
     public GameManager gamemanager;
     GameManager gm;
     bool poking;
@@ -12,10 +13,24 @@ public class StickJab : MonoBehaviour
     {
         poking = gamemanager.poking;
 
+=======
+    public bool poking;
+    public Rigidbody rb;
+    public GameManager gamemanager;
+    public float jumpForce;
+    BoxCollider bc;
+    private void Start()
+    {
+        bc = GetComponent<BoxCollider>();
+    }
+    private void Update()
+    {
+>>>>>>> master
         if (Input.GetMouseButtonDown(0) && !poking)
         {
             StartCoroutine(poke(1.0f, 0.1f));
             poking = true;
+<<<<<<< HEAD
             StartCoroutine(poke(2.0f, 0.08f));
             gamemanager.poke();
             FindObjectOfType<AudioManager>().Play("StickJab");
@@ -24,24 +39,50 @@ public class StickJab : MonoBehaviour
             {
                 transform.localPosition = new Vector3(transform.localPosition.x + 1 * Time.deltaTime, 0, 0);
             }*/
+=======
+            gamemanager.poke();
+>>>>>>> master
         }
     }
     IEnumerator poke(float distance, float time)
     {
         float currentX = transform.localPosition.x;
+<<<<<<< HEAD
 
         for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / time)
         {
             transform.localPosition = new Vector3(Mathf.Lerp(currentX, distance, t), 0, 0);
+=======
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / time)
+        {
+            transform.localPosition = new Vector3(Mathf.Lerp(currentX, distance * 1.2f, t), 0, 0);
+            bc.size = new Vector3(Mathf.Lerp(currentX, distance, t), 0.4f, 0.4f);
+            bc.center = new Vector3(Mathf.Lerp(currentX, distance, t), 0, 0);
+>>>>>>> master
             yield return null;
         }
         if (poking)
         {
             gamemanager.poke();
+<<<<<<< HEAD
             StartCoroutine(poke(0.0f, 0.2f));
         }
     }
 
+=======
+            poking = false;
+            StartCoroutine(poke(0.0f, 0.2f));
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ground" && poking)
+        {
+            rb.AddRelativeForce(Vector3.right * -jumpForce, ForceMode.VelocityChange);
+            Debug.Log("test");
+        }
+    }
+>>>>>>> master
     /*IEnumerator Jab()
     {
         transform.localPosition = new Vector3(1, 0, 0);
