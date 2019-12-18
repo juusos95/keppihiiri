b5208276912785a4a1cceb3e2cede9daee483c5a
TuukkaR <56426883+TuukkaR@users.noreply.github.com>
2019-12-18 12:26:34 +0200

    respawn

---
+++ Assets/Scripts/respawntrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class respawntrigger : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    
+}
