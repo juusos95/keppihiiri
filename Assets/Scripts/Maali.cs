b40584255e9dc1313a50705f7e97e0ae5fddc7d3
Maistelujaska <50902126+Maistelujaska@users.noreply.github.com>
2019-12-19 10:19:37 +0200

    asdfasdf

---
+++ Assets/Scripts/Maali.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Maali : MonoBehaviour
+{
+    [SerializeField] private bool loadSceneByName = false;
+    [SerializeField] private int levelToLoad = 0;
+    public float finishDelay = 1f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!loadSceneByName && other.gameObject.tag == "MaaliCheck")
+        {
+            Time.timeScale = 1f;
+            Invoke("Finish", finishDelay);
+        }
+    }
+
+    void Finish()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
